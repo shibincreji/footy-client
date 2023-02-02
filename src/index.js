653db789dae3705a6e92af39a61387b1121974a0
Shibin C Reji <scr2404@gmail.com>
2023-02-02 19:50:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';11
+import React from 'react';111
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
